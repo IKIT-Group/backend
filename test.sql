1d966673b4db2b5f075ba830f3cc7276df46eb9e
Efimish <efim.ishenin@gmail.com>
2024-05-08 00:48:49 +0700

    schema now actually generated uuid

--- test.sql
+++ test.sql
@@ -1,47 +1,64 @@
+-- PETS
+DELETE FROM "public"."Pet";
+
 INSERT INTO "public"."Pet" (
-    "id", "type", "name", "description",
-    "image", "gender", "sterilized",
-    "hasPassport", "health", "dateOfBirth"
+    "type", "name",
+    "description",
+    "image", "gender", "sterilized", "hasPassport", "health", "dateOfBirth"
 ) VALUES
-(gen_random_uuid(), 'cat', 'Шлепа',
+('cat', 'Шлепа',
 'Ласковый котик, любит ИФИЯК и нюхать мяту',
 'cat.png', true, true, true, 'great', '2022-01-01'
 ),
-(gen_random_uuid(), 'cat', 'Маруся',
+('cat', 'Маруся',
 'Чистоплотная и любвеобильная девочка',
 'cat.png', true, true, true, 'great', '2023-01-01'
 ),
-(gen_random_uuid(), 'dog', 'Джек',
+('dog', 'Джек',
 'Просто хороший мальчик, любит грызть мебель',
 'dog.png', true, true, true, 'great', '2023-01-01'
 ),
-(gen_random_uuid(), 'hamster', 'Виталик',
+('hamster', 'Виталик',
 'Хомячьё - дурачьё',
 'hamster.png', true, true, true, 'great', '2024-03-01'
 );
 
 INSERT INTO "public"."Pet" (
-    "id", "type", "name", "description",
+    "type", "name", "description",
     "image", "gender", "sterilized",
     "hasPassport", "health", "dateOfBirth"
 ) VALUES
-(gen_random_uuid(), 'cat', 'Жорик',
+('cat', 'Жорик',
 'Постоянно голодный, но мы как студенты его понимаем',
 'cat.png', true, true, true, 'great', '2022-01-01'
 ),
-(gen_random_uuid(), 'cat', 'Борик',
+('cat', 'Борик',
 'Постоянно голодный, но мы как студенты его понимаем',
 'cat.png', true, true, true, 'great', '2022-01-01'
 ),
-(gen_random_uuid(), 'cat', 'Жопик',
+('cat', 'Жопик',
 'Постоянно голодный, но мы как студенты его понимаем',
 'cat.png', true, true, true, 'great', '2022-01-01'
 ),
-(gen_random_uuid(), 'cat', 'Анатолий',
+('cat', 'Анатолий',
 'Постоянно голодный, но мы как студенты его понимаем',
 'cat.png', true, true, true, 'great', '2022-01-01'
 ),
-(gen_random_uuid(), 'cat', 'Демьян',
+('cat', 'Демьян',
 'Постоянно голодный, но мы как студенты его понимаем',
 'cat.png', true, true, true, 'great', '2022-01-01'
+);
+
+-- PRODUCTS
+DELETE FROM "public"."Product";
+
+INSERT INTO "public"."Product" (
+    "type", "name",
+    "image", "price", "oldPrice"
+) VALUES
+('food', 'Сухой корм Edel Adult для взрослых стерелизованных кошек, 10 кг.',
+'food.png', 4842, null
+),
+('food', 'Сухой корм Edel Adult для взрослых стерелизованных кошек, 1.5 кг.',
+'food.png', 972, 1050
 );
